c66e50dd7be442a5a5b7cb1717c1e44f17866554
danilocbueno <danilocbueno@gmail.com>
2024-11-09 20:14:32 -0300

    fixing redirect_uri problem

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,6 +1,25 @@
-import { makeRouteHandler } from '@keystatic/next/route-handler';
-import keystaticConfig from '../../../../keystatic.config';
+import { makeRouteHandler } from "@keystatic/next/route-handler";
+import keystaticConfig from "../../../../keystatic.config";
 
-export const { POST, GET } = makeRouteHandler({
+export const { POST: ksPOST, GET: ksGET } = makeRouteHandler({
   config: keystaticConfig,
 });
+
+function rewriteHostname(request: Request) {
+  const url = new URL(request.url);
+  if (process.env.HOST_OVERRIDE) {
+    url.hostname = process.env.HOST_OVERRIDE;
+    url.protocol = "https";
+    url.port = "443";
+  }
+
+  return new Request(url.toString(), request);
+}
+
+export function GET(request: Request) {
+  return ksGET(rewriteHostname(request));
+}
+
+export function POST(request: Request) {
+  return ksPOST(rewriteHostname(request));
+}
